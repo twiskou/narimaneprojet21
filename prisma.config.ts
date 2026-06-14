b1f10389fd16ccda994f411d2205113d22b859e0
twiskou <kheireddinedebz@gmail.com>
2026-06-14 16:16:57 +0100

    simplify prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig } from "prisma/config";
-import { createClient } from "@libsql/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrate: {
-    async adapter() {
-      const libsql = createClient({
-        url: process.env.TURSO_DATABASE_URL!,
-        authToken: process.env.TURSO_AUTH_TOKEN!,
-      });
-      return new PrismaLibSql(libsql);
-    },
-  },
 });
